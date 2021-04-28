eb38e718dbd39da30b6f8b07e9cd116d12014776
Hai Gentle <hoanghaik899@gmail.com>
2021-04-28 17:35:17 +0700

    update nhe ^^!

--- src/components/sideBarItemsCpn/index.jsx
+++ src/components/sideBarItemsCpn/index.jsx
@@ -8,7 +8,7 @@ export default function SideBarItemsCpn(props) {
         <div className="side-bar__items">
             <FontAwesomeIcon icon={icon} style={{ height: "21.5px", width: "21.5px" }} />
             {isOpen && <p className="side-bar__items-text">{text}</p>}
-            <p className="side-bar__items-text --hidden">{text}</p>
+            {!isOpen && <p className="side-bar__items-text --hidden">{text}</p>}
         </div>
     )
 }
